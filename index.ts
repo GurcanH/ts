599c7ea84aabb838ec028b135dd5f841e1520f3f
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-27 11:17:31 +1100

    148. Basic Types

---
+++ index.ts
@@ -0,0 +1,10 @@
+const myName: string = 'Gurcan';
+
+const ten: number = 10;
+const negative: number = -1000;
+const decimal: number = 10.000001;
+
+const yes: boolean = true;
+
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
